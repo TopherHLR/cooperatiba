c04538dce21137b48b4a5e1f43e4ef80a4085b77
TopherHLR <cthilairon@gmail.com>
2025-05-17 02:27:23 +0800

    buy

--- resources/views/items.blade.php
+++ resources/views/items.blade.php
@@ -561,10 +561,10 @@
                     <div class="flex items-center mb-4">
                         <div class="bg-[#047705]/20 p-2 rounded-lg">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-[#047705]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                             </svg>
                         </div>
-                        <h3 class="text-xl font-bold text-white ml-3" id="cartModalTitle">Add to Cart</h3>
+                        <h3 class="text-xl font-bold text-white ml-3" id="cartModalTitle">Buy Now</h3>
                     </div>
                     
                     <hr class="border-[.5px] border-white mb-4 -mx-2">
