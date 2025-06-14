76c792d6310c54283913bad5cc0d337749cef8de
TopherHLR <cthilairon@gmail.com>
2025-06-15 07:07:11 +0800

    dasdw

--- app/Mail/NotificationMessage.php
+++ app/Mail/NotificationMessage.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class NotificationMessage extends Mailable
+class NotificationMessage extends Mailable  implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
